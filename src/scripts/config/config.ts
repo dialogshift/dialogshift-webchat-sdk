18acb7ac5835dd0d87309475557ba840ea52e716
Maxim Dimov <dimovmo@gmail.com>
2019-05-06 17:24:02 +0300

    release v0.0.12-stage.0

--- src/scripts/config/config.ts
+++ src/scripts/config/config.ts
@@ -18,7 +18,7 @@ export const config = {
   teaserCls: 'ds-teaser',
 
   env: {
-    apiEndpoint: 'https://core.prod.co25.net:5001',
-    iframeHost: 'https://webchat.dialogshift.com',
+    apiEndpoint: 'https://core.stage.co25.net:5001',
+    iframeHost: 'https://webchat.stage.co25.net',
   },
 }
