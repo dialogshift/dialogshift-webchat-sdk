1a80904939d2560169d84e943b7c6e061091b14e
Maxim Dimov <dimovmo@gmail.com>
2019-06-28 17:37:01 +0300

    added tests

---
+++ tests/spec/core/utils.spec.ts
@@ -0,0 +1,22 @@
+import { parseUrlParam } from '../../../src/scripts/core/utils'
+import { expect } from 'chai'
+
+describe('Utils', () => {
+  describe('parseUrlParam()', () => {
+    it('should return default param when URL has no requested GET parameter', () => {
+      const url = 'https://www.dialog.com/'
+      const defaultParam = 'default'
+      const param = parseUrlParam(url, 'dschat', defaultParam)
+
+      expect(param).equal(defaultParam)
+    })
+
+    it('should return expected GET param from URL', () => {
+      const value = '1'
+      const url = 'https://www.dialog.com?dschat=1'
+      const param = parseUrlParam(url, 'dschat')
+
+      expect(param).equal(value)
+    })
+  })
+})
