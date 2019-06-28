87b97e637f02bbb172f5cdce81d47717741196f3
Maxim Dimov <dimovmo@gmail.com>
2019-06-28 12:25:01 +0300

    release 0.9.1

--- src/scripts/config/env.dev.js
+++ src/scripts/config/env.dev.js
@@ -1,5 +1,4 @@
 export const environment = {
   apiEndpoint: 'https://core.dev.co25.net:5001',
-  // iframeHost: 'https://webchat.dev.co25.net',
-  iframeHost: 'http://127.0.0.1:8282',
+  iframeHost: 'https://webchat.dev.co25.net',
 }
