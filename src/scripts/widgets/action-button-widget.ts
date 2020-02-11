1ee21d75bc7ce593d7449ffa291695219684a467
Maxim Dimov <dimovmo@gmail.com>
2020-02-11 19:37:57 +0300

    added action button successor push

--- src/scripts/widgets/action-button-widget.ts
+++ src/scripts/widgets/action-button-widget.ts
@@ -1,27 +1,44 @@
 import { BaseWidgetOptions, BaseWidget } from '../core/base-widget'
 import { config } from '../config/config'
+import { ActionButton } from '../models'
+import { App } from '../core/app'
 
 export enum ActionButtonWidgetType {
-  reply = 'reply',
+  quickreply = 'quickreply',
 }
 
 export interface ActionButtonWidgetOptions extends BaseWidgetOptions {
-  id: string
+  actionButton: ActionButton
+  locale: string
+  app: App
 }
 
 export class ActionButtonWidget extends BaseWidget {
-  private type: ActionButtonWidgetType
-  private id: string
+  private actionButton: ActionButton
+  private locale: string
+  private app: App
 
   constructor(options: ActionButtonWidgetOptions) {
     super(options)
+
+    this.setContent(this.actionButton.getTitle(this.locale))
+  }
+
+  private bindEvents() {
+    this.getBoxElem().addEventListener('click', () => {
+      this.app.triggerElement({
+        successor: this.actionButton.getSuccessor()
+      })
+    })
+  }
+
+  render() {
+    super.render()
+
+    this.bindEvents()
   }
 
   getBaseCls() {
     return config.actionButtonCls
-  }
-
-  getId(): string {
-    return this.id
   }
 }
