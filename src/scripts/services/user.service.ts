4bb87877b647acf3eb063fdd5b95ae93799ddfe6
Maxim Dimov <dimovmo@gmail.com>
2020-07-06 17:27:20 +0300

    changed user validation

--- src/scripts/services/user.service.ts
+++ src/scripts/services/user.service.ts
@@ -3,11 +3,11 @@ import { ApiService, CookieService } from './'
 import { parseUrlParam } from '../core/utils'
 
 export class UserService {
-  static touchUser(clientId: string, locale: string): Promise<MixedObject> {
+  static touchUser(clientId: string, locale: string): Promise<string> {
     return new Promise((resolve: any) => {
       let source = 'pwa-embed'
       const currentUserId = CookieService.get('ds-custid')
-      console.log('touchUser', currentUserId)
+
       if (!clientId) {
         throw new Error('Client ID is undefined')
       }
@@ -29,7 +29,7 @@ export class UserService {
             expires: 86400 * 90, // 90 days
           })
 
-          resolve(data)
+          resolve(data.custid)
         })
       } else if (currentUserId) {
         const currentURL = parseUrlParam(window.location.href, 'curl')
@@ -38,23 +38,22 @@ export class UserService {
           clientId,
           currentUserId,
           currentURL,
+        }).then(() => {
+          resolve(currentUserId)
         })
-          .then((data: any) => {
-            resolve(data)
-          })
-          .catch(() => {
-            ApiService.createUser({
-              clientId,
-              source,
-              locale,
-            }).then((data: any) => {
-              CookieService.set('ds-custid', data.custid, {
-                expires: 86400 * 90, // 90 days
-              })
-
-              resolve(data)
+        .catch(() => {
+          ApiService.createUser({
+            clientId,
+            source,
+            locale,
+          }).then((data: any) => {
+            CookieService.set('ds-custid', data.custid, {
+              expires: 86400 * 90, // 90 days
             })
+
+            resolve(data.custid)
           })
+        })
       }
     })
   }
