09ca504807174ca2dc48e4269235bda8b0721389
Maxim Dimov <dimovmo@gmail.com>
2020-08-14 10:54:12 +0300

    changed csrf flow

--- src/scripts/services/analytics.service.ts
+++ src/scripts/services/analytics.service.ts
@@ -2,30 +2,31 @@ import { MixedObject } from '../types'
 import { ApiService, CookieService } from './'
 
 const csrfCookieName = 'ds-csrf'
+const customerIdCookieName = 'ds-custid'
 
 export class AnalyticsService {
   static touchToken(clientId: string): Promise<string> {
     return new Promise((resolve: any) => {
-      const csrftoken = CookieService.get(csrfCookieName)
+      const csrftoken = CookieService.get(customerIdCookieName)
       const sec = btoa(window.screen.width.toString())
 
       if (csrftoken) {
         resolve(csrftoken)
+      } else {
+        ApiService.createToken({
+          clientId,
+          csrftoken,
+          sec,
+        }).then((response: MixedObject) => {
+          if (!csrftoken) {
+            CookieService.set(csrfCookieName, response.csrftoken, {
+              expires: 86400 * 7, // 7 days
+            })
+
+            resolve(response.csrftoken)
+          }
+        })
       }
-
-      ApiService.createToken({
-        clientId,
-        csrftoken,
-        sec,
-      }).then((response: MixedObject) => {
-        if (!csrftoken) {
-          CookieService.set(csrfCookieName, response.csrftoken, {
-            expires: 86400 * 7, // 7 days
-          })
-
-          resolve(response.csrftoken)
-        }
-      })
     })
   }
 
