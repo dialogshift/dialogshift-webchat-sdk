05f5703a0377cb226e04080c314fa2cbba9085fc
Maxim Dimov <dimovmo@gmail.com>
2021-02-23 14:17:26 +0300

    debug uuid

--- src/scripts/services/user.service.ts
+++ src/scripts/services/user.service.ts
@@ -43,7 +43,7 @@ export class UserService {
       if (location.pathname.indexOf('/g/') !== -1) {
         source = 'pwa-c2o'
       }
-
+      console.log(currentUserId, currentUserId === 'null')
       if (!currentUserId) {
         ApiService.createUser({
           clientId,
