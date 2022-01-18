3a1eb6d14ef06a8aaa88c561d0f9102915ec1ca9
Maxim Dimov <dimovMO@gmail.com>
2022-01-18 15:52:33 +0300

    fixed slide

--- src/scripts/core/widget-manager.ts
+++ src/scripts/core/widget-manager.ts
@@ -355,12 +355,13 @@ export class WidgetManager {
   }
 
   renderWhatsappButtonWidget(chatConfig: MixedObject) {
-    const { effects, bwWaButton } = chatConfig
+    const { effects, bwWaButton, slideWaButton } = chatConfig
 
     this.whatsappButtonWidget = new WhatsappButtonWidget({
       renderTo: this.wrapperWidget.getBoxElem(),
       effects: effects?.whatsappButton,
       blackWhiteStyle: bwWaButton,
+      slideWaButton: slideWaButton,
       events: [
         {
           type: 'toggle',
